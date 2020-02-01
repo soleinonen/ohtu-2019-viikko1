3bd562d8fcb12914b6bc2956fb17a4b7d3d48fe4
Sami Leinonen <soleinonen@gmail.com>
2020-02-01 21:02:28 +0200

    Codeclimate config.

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -19,7 +19,8 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else {
+        } else 
+	{
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         this.lisaaVarastoon(alkuSaldo);
